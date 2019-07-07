c9445a9de7f5e235e75801a0fbfb6061add0c4de
aniruddha <aniruddha.kudalkar@gmail.com>
2019-07-07 12:58:08 +0530

    components : view encapsulation

--- angular/my-workspace/projects/bootstrap-pricing/src/app/title-info/title-info.component.ts
+++ angular/my-workspace/projects/bootstrap-pricing/src/app/title-info/title-info.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-title-info',
   templateUrl: './title-info.component.html',
-  styleUrls: ['./title-info.component.css']
+  styleUrls: ['./title-info.component.css'],
+ 
 })
 export class TitleInfoComponent implements OnInit {
 
