9d859d82a12f4b543fd41c4353d5d723d094c8b5
aniruddha <aniruddha.kudalkar@gmail.com>
2019-08-10 11:51:28 +0530

    safe push

--- angular/my-workspace/projects/forms/src/app/dd/my-val.ts
+++ angular/my-workspace/projects/forms/src/app/dd/my-val.ts
@@ -1,4 +1,5 @@
 import { ValidationErrors, AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 export class MyValidator {
 
@@ -10,5 +11,16 @@ export class MyValidator {
                 isA: true
             }
         }
+    }
+
+    static asyncVali(c: AbstractControl): Observable<ValidationErrors | null> {
+        return Observable.create(em => {
+            if (c.value.charAt(0) == 'a' || c.value.charAt(0) == 'A') {
+                em.next(null)
+            } else {
+                em.next({ isA: true })
+            }
+            em.complete()
+        })
     }
 }
